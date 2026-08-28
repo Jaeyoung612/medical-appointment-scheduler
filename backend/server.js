d672c0a9c987bf8b312b2f233edb7b11a54ff59a
jaeyoung612 <jae25239@gmail.com>
2026-08-28 20:18:08 +1000

    feat: connect Node.js backend to MySQL database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db');
 
 const app = express();
 app.use(cors());
@@ -7,6 +8,15 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Sever is running!');
+});
+
+app.get('/test-db', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM dentists');
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 const PORT = 5000;
